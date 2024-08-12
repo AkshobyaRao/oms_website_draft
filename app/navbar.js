6f5970a6d60ac33c540eb39a4615e359ecac32cd
Akshobya Rao <AverageGoku@outlook.com>
2024-08-11 19:12:04 -0500

    Additional Changes
    Need approval/further assests and designs for further commits.

--- app/navbar.js
+++ app/navbar.js
@@ -1,40 +1,37 @@
-import "../app/globals.css"
-import { useEffect, useState} from "react";
-import Image from "../public/logo.png"
+import "../app/globals.css";
+import { useEffect, useState } from "react";
+import Image from "../public/logo.png";
 export default function Home() {
-  const [scrl, setScrl] = useState(false)
+  const [scrl, setScrl] = useState(false);
   useEffect(() => {
     const handleScrl = () => {
-      setScrl(window.scrollY > 200)
-    }
-    window.addEventListener('scroll', handleScrl)
-    return () => window.addEventListener('scroll', handleScrl)
-  })
+      setScrl(window.scrollY > 200);
+    };
+    window.addEventListener("scroll", handleScrl);
+    return () => window.addEventListener("scroll", handleScrl);
+  });
   return (
     <header className={`${scrl ? "sticky" : ""}`}>
       <nav className="nav">
-        <span>
-            Ojas
-        </span>
+        <span>⚕️</span>
         <ul>
-            <li onClick={() => window.scrollTo({ top: 500, behavior: "smooth" })}>
-                About Us
-            </li>
-            <li onClick={() => window.scrollTo({ top: 1000, behavior: "smooth" })}>
-                Our Services
-            </li>
-            <li>
-                Clients
-            </li>
-            <li>
-                Testimonials
-            </li>
-            <button onClick={() => window.scrollTo({ top: 1500, behavior: "smooth" })}>
-                Contact Us
-            </button>
+          <li onClick={() => window.scrollTo({ top: 500, behavior: "smooth" })}>
+            About Us
+          </li>
+          <li
+            onClick={() => window.scrollTo({ top: 1000, behavior: "smooth" })}
+          >
+            Our Services
+          </li>
+          <li>Clients</li>
+          <li>Testimonials</li>
+          <button
+            onClick={() => window.scrollTo({ top: 1500, behavior: "smooth" })}
+          >
+            Contact Us
+          </button>
         </ul>
-        
       </nav>
-    </header>  
+    </header>
   );
 }
